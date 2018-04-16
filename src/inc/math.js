222fa45f3d6d603e8f7b782912d671e785a93beb
Tomas Cikel <xcikel00@vutbr.cz>
2018-04-16 13:25:50 +0200

    All mathematical functions expect the "extra" function

--- src/inc/math.js
+++ src/inc/math.js
@@ -1,13 +1,12 @@
 // Math library
 // Passing the value as a String
 
-str 
-var operand = ['+','-','*','/']
 
-for(var i = 0; i<str.length; i++){ 
-    isNaN(i);
 
-}
+
+
+
+
 
 
 
@@ -21,58 +20,121 @@ function TQsub(a,b){
 } 
 
 
-
+/*
+@brief If var "b"is 0 throws an exepction
+*/
 function TQdiv(a,b){ 
+    if (b==0){
+        throw exception;
+        return 0;
+    }
+    else { 
     return a/b;
+    }
 } 
+
 
 
 function TQmul(a,b){ 
     return a*b;
 }
 
-function TQfact(a){   //return factorial of an var a
-    for(var i=a; i>1;--i){ 
-        a=a*i;
+
+
+
+function TQfact(a){  
+    if (a<0){ 
+    throw exception;
+        return 0;
+    } 
+    else { 
+    for(var i=0; i<a;i++){ 
+        a=a*(a-i);
+    
     }
+    }  
     return a;
 
 }
 
+
+/*
+ @brief Power function: take var "a" and "n" as input.
+        Var "a" is the value which functio powers with var "n" 
+*/
+
 function TQpow(a,n){
-    var b;
-    for( var i=n; i>1;i--){ 
-     b=a*a;
- }
+    
+    if(n>0){ 
+    var b=a;
+        for( var i=n; i>1;i--){ 
+            b=b*a;
+        }
+    } 
+
+    else if (n<0){ 
+    var b=a;
+        for ( var i=Math.abs(n); i>-1;i++){ 
+            b=b*a;
+        }
+        b=1/b;
+    }
+
+    else {
+        b=1;
+    }
+
+         
+
  return b;
 }
 
-function TQsqrt(a){  //getting sqrt of var "a" using newton's method
+
+
+/*
+  @brief Sqrt function using Newton's method
+  Returns exception if a input is a negative number
+  Stops the calculation if a number of it
+ */
+
+function TQsqrt(a){  
  var guess= Math.ceil(a/2);
  var nextguess;
- var 
+ var num_itr=0;
 
- while(nextguess.length < 10){ 
+ if(a<0){ 
+     throw exception;
+     return 0;
+ }
+ else {
+ while((''+guess).length < 8 || num_itr<8 ){ 
 
-nextguess=guess - (TQpow(guess,2)/derative);
-guess=nextguess;
-
-
-
-function derative(x){
-    
-    var dx = dx || .00001;
-    var p= TQpow(dx+x,2);
-    return (p-TQpow(x))/dx;
-
-}
-
-
-
+        nextguess=guess - ((TQpow(guess,2)-a)/(2*(guess)));
+        guess=nextguess;
+        num_itr++;
+ } 
+}  
 return nextguess;
 
+}
 
- } 
+/*
+@todo Function logarithm
+*/
+
+function TQlog(a){ 
+       
 }
 
 
+
+module.exports = {
+    TQadd: TQadd,
+    TQsub: TQsub,
+    TQmul: TQmul,
+    TQdiv: TQdiv,
+    TQfact: TQfact,
+    TQpow: TQpow,
+    TQsqrt: TQsqrt,
+
+};
